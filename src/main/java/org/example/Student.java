841d869352325d101201ebce8c2eec2d2f925c5c
KamilLoska <72936376+KamilLoska@users.noreply.github.com>
2023-06-19 20:57:42 +0200

    Add files via upload

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Student {
+
+    enum Status {
+        SLACKER, REGULAR, OUTSTANDING
+    }
+
+    private final Status status;
+
+    Student(Status status) {
+        this.status = status;
+    }
+
+    boolean isSlacker() {
+        return status == Status.SLACKER;
+    }
+
+    boolean isOutstanding() {
+        return status == Status.OUTSTANDING;
+    }
+}
